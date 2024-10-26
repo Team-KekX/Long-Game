97b3cf9c8269f84b4db4f8ad0a10dfbc4551ebe9
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-10-26 19:05:11 +0200

    Add Spring Backend

---
+++ jserver/src/main/java/eu/kekx/long_game/LongGameServerApplication.java
@@ -0,0 +1,13 @@
+package eu.kekx.long_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongGameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongGameServerApplication.class, args);
+	}
+
+}
---
+++ jserver/src/test/java/eu/kekx/long_game/LongGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.kekx.long_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongGameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

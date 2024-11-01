edbe7e7a5236770c7a9c3587b6d5fc7bdff5fe77
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-11-01 13:48:03 +0100

    (jserver) Add Password Encoding to User

--- jserver/src/main/java/eu/kekx/long_game/domain/User.java
+++ jserver/src/main/java/eu/kekx/long_game/domain/User.java
@@ -1,11 +1,19 @@
 package eu.kekx.long_game.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Objects;
 
 @Setter
 @Getter
-@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 
@@ -21,4 +29,30 @@ public class User extends AbstractEntity {
 
     @Column(name = "password", nullable = false)
     private String password;
+
+    public User(String username, String email, String password, PasswordEncoder passwordEncoder) {
+        this.username = username;
+        this.email = email;
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public boolean comparePasswords(String rawPassword, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(rawPassword, this.password);
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
